3e45b347d356145226a340b551293c08828883be
italosanches <italo_carvalho157@hotmail.com>
2024-07-15 15:22:42 -0300

    Adicao de JsonOptions

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,12 +1,15 @@
 using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => 
+												 options.JsonSerializerOptions.ReferenceHandler = 
+												 ReferenceHandler.IgnoreCycles);//option for ignore cicles
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
