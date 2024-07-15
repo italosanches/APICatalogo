5736f965da09885e48263f34e9858b885959c581
italosanches <italo_carvalho157@hotmail.com>
2024-07-15 15:22:09 -0300

    Adição de try catch

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,125 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriasController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+		public CategoriasController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet("produtos")]
+		public ActionResult<IEnumerable<Categoria>> GetCategoriasAndProdutos()
+		{
+			try
+			{
+				return _context.Categorias.AsNoTracking().Include(b => b.Produtos).ToList();
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atenter a solicitação");
+			}
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Categoria>> Get()
+		{
+			try
+			{
+				IEnumerable<Categoria> categorias = _context.Categorias.AsNoTracking().ToList();
+				if (!categorias.Any() || categorias is null)
+				{
+					return NotFound("Não há categorias.");
+				}
+				return Ok(categorias);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atenter a solicitação.");
+			}
+		}
+
+		[HttpGet("{id:int}", Name = "GetCategoriaById")]
+		public ActionResult<Categoria> Get(int id)
+		{
+			try
+			{
+				var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(x => x.CategoraId == id);
+				if (categoria == null)
+				{
+					return NotFound("Categoria não encontrada.");
+				}
+				return Ok(categoria);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atenter a solicitação.");
+			}
+		}
+
+		[HttpPost]
+		public ActionResult<Categoria> Post([FromBody] Categoria categoria)
+		{
+			try
+			{
+				if (categoria is null)
+				{
+					return BadRequest();
+				}
+				_context.Categorias.Add(categoria);
+				_context.SaveChanges();
+				return new CreatedAtRouteResult("GetCategoriaById", new { id = categoria.CategoraId }, categoria);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
+		}
+		[HttpPut("{id:int}")]
+		public ActionResult Put(int id, [FromBody] Categoria categoria)
+		{
+			try
+			{
+				if (id != categoria.CategoraId)
+				{
+					return BadRequest();
+				}
+				_context.Entry(categoria).State = EntityState.Modified;
+				_context.SaveChanges();
+				return Ok(categoria);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
+		}
+
+		[HttpDelete("{id:int}")]
+		public ActionResult Delete(int id)
+		{
+			try
+			{
+				var categoria = _context.Categorias.FirstOrDefault(x => x.CategoraId == id);
+				if (categoria is null)
+				{
+					return NotFound("Categoria não encontrada.");
+				}
+				_context.Categorias.Remove(categoria);
+				_context.SaveChanges();
+				return Ok(categoria);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
+		}
+	}
+}
--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -11,68 +11,103 @@ namespace APICatalogo.Controllers
 	public class ProdutosController : ControllerBase
 	{
 		private readonly AppDbContext _context;
-        public ProdutosController(AppDbContext context)
-        {
-			_context = context; 
-        }
+		public ProdutosController(AppDbContext context)
+		{
+			_context = context;
+		}
 		[HttpGet]
 		public ActionResult<IEnumerable<Produto>> Get()  //ActionResult, permite retornar um Ienumerable ou qualquer tipo de retorno de Actiom Result 
 		{
-			var produtos =  _context.Produtos.AsNoTracking().ToList();
-			if(produtos is null) 
+			try
 			{
-				return NotFound("produtos nao encontrados");
+				var produtos = _context.Produtos.AsNoTracking().ToList();
+				if (produtos is null)
+				{
+					return NotFound("produtos nao encontrados");
+				}
+				return Ok(produtos);
 			}
-			return Ok(produtos);
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
 		}
 
-		[HttpGet("{id:int}", Name= "GetProdutoById")]// parametro que ira chegar no request, e sera  enviado a action ou seja URL
+		[HttpGet("{id:int}", Name = "GetProdutoById")]// parametro que ira chegar no request, e sera  enviado a action ou seja URL
 		public ActionResult<Produto> Get(int id)
 		{
-			var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
-			if (produto is null)
+			try
 			{
-				return NotFound("produto nao encontrado");
+				var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+				if (produto is null)
+				{
+					return NotFound("produto nao encontrado");
+				}
+				return Ok(produto);
 			}
-			return Ok(produto);
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
 		}
 
 		[HttpPost]
-		public ActionResult Post(Produto produto) //Nao retorna nada, o retorno sera feito no CreatedAtRouteResult, produto e enviado no corpo do Post
-		{ 
-			if(produto is null)
+		public ActionResult Post([FromBody] Produto produto) //Nao retorna nada, o retorno sera feito no CreatedAtRouteResult, produto e enviado no corpo do Post
+		{
+			try
 			{
-				return BadRequest();
-			}
-			_context.Produtos.Add(produto);
-			_context.SaveChanges();
+				if (produto is null)
+				{
+					return BadRequest();
+				}
+				_context.Produtos.Add(produto);
+				_context.SaveChanges();
 
-			return new CreatedAtRouteResult("GetProdutoById",new {id = produto.ProdutoId},produto);//retorna 201, e o id do produto criado e o produto
+				return new CreatedAtRouteResult("GetProdutoById", new { id = produto.ProdutoId }, produto);//retorna 201, e o id do produto criado e o produto
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
 		}
 
 		[HttpPut("{id:int}")]
-		public ActionResult Put(int id,Produto produto) 
+		public ActionResult Put(int id, [FromBody] Produto produto)
 		{
-			if(id != produto.ProdutoId)
+			try
 			{
-				return BadRequest();
+				if (id != produto.ProdutoId)
+				{
+					return BadRequest();
+				}
+				_context.Entry(produto).State = EntityState.Modified; // o produto veio de um estado desconectado
+																	  //É necessario falar ao EF que produto esta sendo modificado
+				_context.SaveChanges();
+				return Ok(produto);
 			}
-			_context.Entry(produto).State = EntityState.Modified; // o produto veio de um estado desconectado
-																  //É necessario falar ao EF que produto esta sendo modificado
-			_context.SaveChanges();
-			return Ok(produto);
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
 		}
 		[HttpDelete("{id:int}")]
-		public ActionResult Delete(int id) 
+		public ActionResult Delete(int id)
 		{
-			var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-			if(produto is null)
+			try
 			{
-				return NotFound("Produto não localizado");
+				var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+				if (produto is null)
+				{
+					return NotFound("Produto não localizado");
+				}
+				_context.Produtos.Remove(produto);
+				_context.SaveChanges();
+				return Ok();
 			}
-			_context.Produtos.Remove(produto);
-			_context.SaveChanges();
-			return Ok();
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "erro ao atender a solicitação.");
+			}
 		}
 
 	}
